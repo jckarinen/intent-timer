287f005c7b83e70b1569c4a5166c0a5c9f99255c
jckarinen <91632899+jckarinen@users.noreply.github.com>
2025-12-01 18:37:51 -0700

    [refactor] consolidate program into intent.c

--- intent.c
+++ intent.c
@@ -4,6 +4,28 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+void setTimer(int min) {
+	int dur = min * 60;
+	int dur1 = dur > 60 ? dur - 60 : 0;
+	int dur2 = dur > 60 ? 60 : dur;
+	sleep(dur1);
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror("Fork failed (aux)");
+		exit(1);
+	}
+	else if (pid == 0) {
+		execl("/usr/bin/osascript", "osascript", "-e", "display notification \"60 seconds remaining before screen lock\" with title \"Time's almost up\"", NULL);
+		perror("exec failed");
+		exit(1);
+	}
+	else {
+		sleep(dur2);
+		execl("/usr/bin/osascript", "osascript", "-e", "tell application \"System Events\" to keystroke \"q\" using {control down, command down}", NULL);
+	}
+	return;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("Too few arguments\n");
@@ -16,7 +38,7 @@ int main(int argc, char *argv[]) {
 
 	char *endptr;
 	errno = 0;
-	strtol(argv[1], &endptr, 10);
+	int n = strtol(argv[1], &endptr, 10);
 
 	if (errno == ERANGE) {
 		printf("Integer out of range: %s\n", argv[1]);
@@ -36,10 +58,7 @@ int main(int argc, char *argv[]) {
 	}
 	else if (pid == 0) {
 		setsid();
-		execl("./timer", "timer", argv[1], NULL);
-
-		perror("Exec failed");
-		exit(1);
+		setTimer(n);
 	}
 
 	return 0;
